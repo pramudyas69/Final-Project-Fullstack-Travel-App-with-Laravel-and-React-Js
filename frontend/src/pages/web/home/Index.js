8219167d73df5f7d08649a27cc3a00f3dc69b21b
pramudyas69 <18102280@ittelkom-pwt.ac.id>
2022-07-30 23:57:54 +0700

    first commit

---
+++ frontend/src/pages/web/home/Index.js
@@ -0,0 +1,24 @@
+//import layout
+import React from "react";
+
+//import layout web
+import LayoutWeb from "../../../layouts/Web";
+
+//import Slider component
+import Slider from "../../../components/web/Slider";
+
+function Home() {
+  return (
+    <React.Fragment>
+      <LayoutWeb>
+        <Slider />
+
+        <div className="container mt-5">
+          <h1>Home</h1>
+        </div>
+      </LayoutWeb>
+    </React.Fragment>
+  );
+}
+
+export default Home;
